9196cbd8b6f7ed5733d07418fdeb1419f73ec09c
LAPTOP-VSEGIAFO\surya <suryachamarthi9@gmail.com>
2022-02-07 22:11:27 -0500

    impletented login for methods

---
+++ src/com/automation/corejava/lerning/CallDevition.java
@@ -0,0 +1,14 @@
+package com.automation.corejava.lerning;
+
+public class CallDevition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Methods m = new Methods();
+		int c = m.devition(10, 10);
+		System.out.println("Devition of Two numbers! "+c);
+		
+	}
+
+}
---
+++ src/com/automation/corejava/lerning/Methods.java
@@ -0,0 +1,11 @@
+package com.automation.corejava.lerning;
+
+public class Methods {
+
+	public int devition(int a ,int b) {
+		return a/b ;
+	}
+ 
+	
+
+}
