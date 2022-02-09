5d24aabae130cd869329d6fd8e940bad781c161e
LAPTOP-VSEGIAFO\surya <suryachamarthi9@gmail.com>
2022-02-08 19:33:18 -0500

    implemented login for sum

---
+++ src/com/automation/corejava/lerning/CallAdd.java
@@ -0,0 +1,12 @@
+package com.automation.corejava.lerning;
+
+public class CallAdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MethodsAdd m = new MethodsAdd();
+		int c= m.Sum(10, 10);
+		System.out.println("sum of Two numbers! "+c);
+	}
+
+}
