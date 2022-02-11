4953b2fca655c17f8d1dd2439d96305f82a5059d
LAPTOP-VSEGIAFO\surya <suryachamarthi9@gmail.com>
2022-02-10 21:14:29 -0500

    implemented Multy logic

---
+++ CallMulty.java
@@ -0,0 +1,14 @@
+package com.automation.corejava.lerning;
+
+public class CallMulty {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+  
+		MethodsMulty m = new MethodsMulty();
+		int c= m.Multy(10, 10);
+		System.out.println("sum of Two numbers! "+c);
+	}
+
+}
